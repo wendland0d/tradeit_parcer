be4ce5fdf89296523c180bb8f21005fda5247ba8
Daniel Wendland <wendland0d@MacBook-Pro-Daniel.local>
2023-03-27 21:32:16 +0300

    /

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def parse_cinv():
         'Accept-Encoding' : 'gzip, deflate, br',
         'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Safari/605.1.15'
     }
-    session = steam_login(LOGIN='jaksonfeed', PASSWORD='jesus1337christT')
+    session = steam_login(LOGIN='', PASSWORD='')
 
     temp_html = session.get('https://connect-tradeit.com/auth/steam?sendTo=https://tradeit.gg/api/v2/steam/login', headers=headers).content
     soup = BeautifulSoup(temp_html)
