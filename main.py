8715d7d50e85e557682f592783f70a1b009f89c8
Daniel Wendland <wendland0d@MacBook-Pro-Daniel.local>
2023-03-27 21:31:32 +0300

    /

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 import json
 import time
 
+from lxml import html
+from bs4 import BeautifulSoup
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -94,16 +97,22 @@ def parse_cinv():
         'Accept-Encoding' : 'gzip, deflate, br',
         'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Safari/605.1.15'
     }
+    session = steam_login(LOGIN='jaksonfeed', PASSWORD='jesus1337christT')
 
-    session = steam_login(LOGIN='steam_login', PASSWORD='steam_password')
-    session.get('https://steamcommunity.com/openid/login?openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.sreg=http%3A%2F%2Fopenid.net%2Fextensions%2Fsreg%2F1.1&openid.sreg.optional=nickname%2Cemail%2Cfullname%2Cdob%2Cgender%2Cpostcode%2Ccountry%2Clanguage%2Ctimezone&openid.ns.ax=http%3A%2F%2Fopenid.net%2Fsrv%2Fax%2F1.0&openid.ax.mode=fetch_request&openid.ax.type.fullname=http%3A%2F%2Faxschema.org%2FnamePerson&openid.ax.type.firstname=http%3A%2F%2Faxschema.org%2FnamePerson%2Ffirst&openid.ax.type.lastname=http%3A%2F%2Faxschema.org%2FnamePerson%2Flast&openid.ax.type.email=http%3A%2F%2Faxschema.org%2Fcontact%2Femail&openid.ax.required=fullname%2Cfirstname%2Clastname%2Cemail&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.return_to=https%3A%2F%2Fconnect-tradeit.com%2Fauth%2Fsteam%2Freturn%3FsendTo%3Dhttps%3A%2F%2Ftradeit.gg%2Fapi%2Fv2%2Fsteam%2Flogin&openid.realm=https%3A%2F%2Fconnect-tradeit.com%2F', headers=headers)
-    session.get('https://connect-tradeit.com/auth/steam/return?sendTo=https://tradeit.gg/api/v2/steam/login&openid.ns=http://specs.openid.net/auth/2.0&openid.mode=id_res&openid.op_endpoint=https://steamcommunity.com/openid/login&openid.claimed_id=https://steamcommunity.com/openid/id/76561198139581989&openid.identity=https://steamcommunity.com/openid/id/76561198139581989&openid.return_to=https://connect-tradeit.com/auth/steam/return?sendTo=https://tradeit.gg/api/v2/steam/login&openid.response_nonce=2023-03-27T12:38:05ZmB7BQaG93vwWQaM10Mn7L7uFHCI=&openid.assoc_handle=1234567890&openid.signed=signed,op_endpoint,claimed_id,identity,return_to,response_nonce,assoc_handle&openid.sig=2eXPDO3x0UV5OpQX0NAaHgkv8u0=', headers=headers)
-    #session.get('https://tradeit.gg/api/v2/steam/login?id=76561198139581989&ihash=6c620f1d-7af0-4cee-a5ac-89e95ed410aa&shash=ef291700-606d-4426-a198-3eb89b99dc4c', headers=login_header)
-    print(session.get('https://tradeit.gg/api/v2/user/get-intercom-hash',headers=headers).content)
-
-# user_items()
-
-# user_items()
+    temp_html = session.get('https://connect-tradeit.com/auth/steam?sendTo=https://tradeit.gg/api/v2/steam/login', headers=headers).content
+    soup = BeautifulSoup(temp_html)
+    data = {'action':soup.find('input', {'name':'action'}).get('value'),
+            'openid.mode':soup.find('input', {'name':'openid.mode'}).get('value'),
+            'opeindparams':soup.find('input', {'name':'openidparams'}).get('value'),
+            'nonce':soup.find('input', {'name':'nonce'}).get('value')}
+    
+    
+    
+    #print(session.post(f'https://steamcommunity.com/openid/login?action={openid_action}&openid.mode={openid_mode}&openidparams={openid_params}&nonce={openid_nonce}', headers=headers).content)
+    session.post('https://steamcommunity.com/openid/login', json=data)
+    my_items = session.get('https://tradeit.gg/api/v2/inventory/my/data?gameId=753&fresh=0', headers=headers).content
+    print(my_items)
+    
 
 
 parse_cinv()
